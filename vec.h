bf9104186d0b23078ee582dfa0b93f365a6e6cb2
Cristian Marcos Martin <cristianmarcosmartin1@gmail.com>
2025-10-04 22:37:50 +0200

    Added vec_free_deep, vec_get_ptr, vec_get_as, vec_print, documentation and fixed some bugs

--- vec.h
+++ vec.h
@@ -1,3 +1,13 @@
+// vec.h - A vector implementation that stores generic pointers.
+//
+// All functions return a value for error handling:
+// - int: 0 on failure, 1 on success.
+// - void *: NULL on failure, void * on success.
+//
+// Useful macros:
+// - VEC_IMPLEMENTATION: implementations of the Vec structure.
+// - VEC_CAPACITY_STEP: used to initialize and realloc the Vec's capacity.
+
 #ifndef VEC_H_
 #define VEC_H_
 
@@ -6,22 +16,14 @@
 #include <string.h>
 #include <stddef.h>
 
-// Used to initialize and realloc the Vec's capacity
-#ifndef VEC_INITIAL_CAP 
-#define VEC_INITIAL_CAP 4
-#endif // VEC_INITIAL_CAP 
-
-/* Dynamic array that holds opaque elements */
+/* Dynamic array that holds opaque elements. */
 typedef struct Vec {
     /* Array of opaque elements.*/
     void *items;
-
     /* Size of a Vec item.*/
     size_t item_size;
-
     /* How many items the Vec is holding.*/
     size_t size;
-
     /* How many items the Vec can hold.*/
     size_t capacity;
 } Vec;
@@ -29,8 +31,11 @@ typedef struct Vec {
 /* Creates a new Vec. Uses `item_size` to `malloc` items. */
 Vec *vec_new(size_t item_size);
 
-/* Frees `v->items` and `v`. */
-void vec_free(Vec *v);
+/* Frees `v->items` and `v`. To free strings, use `vec_free_deep`. */
+int vec_free(Vec *v);
+
+/* Frees `v->items` (iterating over each element) and `v`. */
+int vec_free_deep(Vec *v);
 
 /* Adds an `item` at the end of `v->items`. */
 int vec_push(Vec *v, void *item);
@@ -38,42 +43,91 @@ int vec_push(Vec *v, void *item);
 /* Adds `src->items` at the end of `dst->items`. */
 int vec_extend(Vec *dst, Vec *src);
 
-/* Returns an item (NULL if out of bounds) using `index`. */
+/**
+ * Returns a pointer to the element at `index`.
+ * Returns `NULL` if `index` is out of bounds or `v->size == 0`.
+ */
 void *vec_get(Vec *v, size_t index);
+
+/**
+ * Returns a pointer to the element's value at `index`.
+ * Returns `NULL` if `index` is out of bounds or `v->size == 0`.
+ */
+void *vec_get_ptr(Vec *v, size_t index);
+
+/**
+ * Returns the value at `i` casted to `type`.
+ * If `vec_get` returns `NULL`, casting it may cause undefined behaviour.
+ */
+#define vec_get_as(v, i, type) (*(type *)vec_get((v), (i)))
+
+/* Prints each item in `v`. */
+int vec_print(Vec *v);
 
 #endif // VEC_H_
 
-// Implementations of the Vec structure.
 #ifdef VEC_IMPLEMENTATION
 
+#ifndef VEC_CAPACITY_STEP
+#define VEC_CAPACITY_STEP 4
+#endif // VEC_CAPACITY_STEP
+
 Vec *vec_new(size_t item_size) {
-    Vec *v = malloc(sizeof(Vec));
+    if (VEC_CAPACITY_STEP < 0) {
+        fprintf(stderr, "VEC_CAPACITY_STEP can't be negative");
+        return NULL;
+    }
+
+    Vec *v = (Vec *)malloc(sizeof(Vec));
+
+    if (!v) {
+        fprintf(stderr, "malloc on vec_new: initialize vector");
+        return NULL;
+    }
 
     v->size = 0;
-    v->capacity = VEC_INITIAL_CAP;
+    v->capacity = VEC_CAPACITY_STEP;
     v->item_size = item_size;
     v->items = malloc(v->capacity * v->item_size);
 
     if (!v->items) {
-        perror("malloc on vec_new");
+        fprintf(stderr, "malloc on vec_new: initialize items");
         return NULL;
     }
 
     return v;
 }
 
-void vec_free(Vec *v) {
+int vec_free(Vec *v) {
+    if (!v) return 0;
+
     free(v->items);
     free(v);
+
+    return 1;
+}
+
+int vec_free_deep(Vec *v) {
+    if (!v) return 0;
+
+    for (size_t i = 0; i < v->size; i++)
+        free(*(void **)vec_get(v, i));
+
+    return vec_free(v);
 }
 
 int vec_push(Vec *v, void *item) {
     if (v->size == v->capacity) {
-        v->capacity += VEC_INITIAL_CAP;
+        if (VEC_CAPACITY_STEP < 0) {
+            fprintf(stderr, "VEC_CAPACITY_STEP can't be negative");
+            return 0;
+        }
 
-        void *tmp = realloc(v->items, v->capacity * v->item_size);     
+        v->capacity += VEC_CAPACITY_STEP;
+
+        void *tmp = realloc(v->items, v->capacity * v->item_size);
         if (!tmp) {
-            perror("realloc on vec_push");
+            fprintf(stderr, "realloc on vec_push");
             return 0;
         }
 
@@ -92,29 +146,32 @@ int vec_push(Vec *v, void *item) {
 }
 
 int vec_extend(Vec *dst, Vec *src) {
+    if (src->size == 0) {
+        fprintf(stderr, "src vector is uninitialized");
+        return 0;
+    }
+
     if (dst->item_size != src->item_size) {
-        perror("Item size must be the same for both vectors");
+        fprintf(stderr, "Item size must be the same for both vectors");
         return 0;
     }
 
     size_t totalSize = dst->size + src->size;
-    
+
     if (totalSize > dst->capacity) {
-        size_t newCapacity = (dst->capacity + src->size);
-        void *tmp = realloc(dst->items, newCapacity * dst->item_size);
+        void *tmp = realloc(dst->items, totalSize * dst->item_size);
 
         if (!tmp) {
-            perror("realloc on vec_extend");
+            fprintf(stderr, "realloc on vec_extend");
             return 0;
         }
 
         dst->items = tmp;
-        dst->capacity = newCapacity;
     }
 
     memcpy(
-        (char *)dst->items + src->size * dst->item_size,
-        (char *)src->items,
+        (char *)dst->items + dst->size * dst->item_size,
+        src->items,
         src->size * dst->item_size
     );
 
@@ -124,12 +181,33 @@ int vec_extend(Vec *dst, Vec *src) {
 }
 
 void *vec_get(Vec *v, size_t index) {
+    if (v->size == 0) {
+        fprintf(stderr, "Vector is uninitialized");
+        return NULL;
+    }
+
     if (index >= v->size) {
         fprintf(stderr, "Index %zu out of bounds (size %zu)", index, v->size);
         return NULL;
     }
 
-    return v->items + index * v->item_size;
+    return ((char *)v->items + index * v->item_size);
+}
+
+void *vec_get_ptr(Vec *v, size_t index) {
+    return *(void **)vec_get(v, index);
+}
+
+int vec_print(Vec *v) {
+    if (v->size == 0) {
+        fprintf(stderr, "Vector is uninitialized");
+        return 0;
+    }
+
+    for (size_t i = 0; i < v->size; i++)
+        printf("%d %s\n", i, *(char **)vec_get(v, i));
+
+    return 1;
 }
 
 #endif // VEC_IMPLEMENTATION

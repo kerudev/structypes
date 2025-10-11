6eb9ce051363358fd268f327b70ad20eaf73f28b
Cristian Marcos Martin <cristianmarcosmartin1@gmail.com>
2025-10-11 03:16:09 +0200

    tree.h: added documentation

--- tree.h
+++ tree.h
@@ -11,29 +11,108 @@
 #include <stdlib.h>
 #include <stdbool.h>
 
+/** A node. */
 typedef struct Node {
-    struct Node *parent;
-    struct Node **nodes;
+    /** Value of the node. */
     void        *value;
+    /** Pointer to the parent node. */
+    struct Node *parent;
+    /** Array of children nodes. */
+    struct Node **nodes;
+    /** Number of child nodes. */
     size_t      size;
 } Node;
 
+/**
+ * A node tree.
+ *
+ * The only difference with `Node` is that `Tree` instances should not have a
+ * `parent` node. When a `parent` is defined, the `Tree` instance should be
+ * treated as a `Node` instance.
+ */
 typedef Node Tree;
 
-// TODO documentation
 // TODO error handling messages
 
+/**
+ * Creates a new `Tree` with a `value`.
+ * See the documentation of `Tree` to know its differences with `Node`.
+ *
+ * Returns `NULL` when:
+ * - `malloc` fails to allocate a new `Tree`.
+ */
 Tree *tree_new(void *value);
-Node *node_new(Tree *parent, void *value);
 
+/**
+ * Creates a new `Node` with a `parent` and a `value`.
+ * See the documentation of `Tree` to know its differences with `Node`.
+ *
+ * Returns `NULL` when:
+ * - `malloc` fails to allocate a new `Node`.
+ */
+Node *node_new(Node *parent, void *value);
+
+/**
+ * Returns `false` when:
+ * - `realloc` for `node->nodes` fails.
+ * - `node_new` fails.
+ */
 bool node_new_child(Node *node, void *value);
+
+/**
+ * Returns `false` when:
+ * - `realloc` for `node->nodes` fails.
+ */
 bool node_add_child(Node *node, Node *child);
 
+/**
+ * A "deep" print of a `Node`.
+ *
+ * Prints the value of `tree` and recurses over each child node (and their
+ * child nodes too).
+ * 
+ * Use `node_print` to print just the immediate child nodes of `tree`.
+ *
+ * Returns `false` when:
+ * - `tree` evaluates to false.
+ */
 bool tree_print(Tree *tree);
+
+/**
+ * A "shallow" print of a `Node`.
+ *
+ * Prints the value of `node` and the value of its immediate child nodes, but
+ * it doesn't recurse into deeper nodes.
+ *
+ * Use `tree_print` to print deeper nodes of `node`.
+ *
+ * Returns `false` when:
+ * - `node` evaluates to false.
+ */
 bool node_print(Node *node);
+
+/**
+ * Prints the values of `parent`, `value` and `size` of `node`.
+ *
+ * Returns `false` when:
+ * - `node` evaluates to false.
+ */
 bool node_info(Node *node);
 
+/**
+ * Frees `tree` and `tree->nodes`.
+ *
+ * Returns `false` when:
+ * - `tree` or `tree->nodes` evaluate to false.
+ */
 bool tree_free(Tree *tree);
+
+/**
+ * Frees `node` and child nodes if `node->size > 0`.
+ *
+ * Returns `false` when:
+ * - `node` evaluates to false.
+ */
 bool node_free(Node *node);
 
 #endif // TREE_H_
@@ -48,24 +127,24 @@ Tree *tree_new(void *value) {
         return NULL;
     }
 
+    tree->value = value;
     tree->parent = NULL;
     tree->nodes = NULL;
-    tree->value = value;
     tree->size = 0;
 
     return tree;
 }
 
-Node *node_new(Tree *parent, void *value) {
+Node *node_new(Node *parent, void *value) {
     Node *node = (Node *)malloc(sizeof(Node));
     if (!node) {
         printf("node_new: malloc error on initialize root");
         return NULL;
     }
 
+    node->value = value;
     node->parent = parent;
     node->nodes = NULL;
-    node->value = value;
     node->size = 0;
 
     return node;

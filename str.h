d2a7e7a9375ca8a49dab8239ed35d43258adeedd
Cristian Marcos Martin <cristianmarcosmartin1@gmail.com>
2025-10-12 20:20:09 +0200

    str.h: added str_len and str_clone

--- str.h
+++ str.h
@@ -14,11 +14,28 @@
 #include <stddef.h>
 
 /**
+ * `strlen` replacement. Returns the size of `s`.
+ * 
+ * Returns `0` when:
+ * - `s` evaluates to false.
+ */
+size_t str_len(char *s);
+
+/**
+ * `strdup` replacement. Returns a clone of `s`.
+ * 
+ * Returns `NULL` when:
+ * - `s` evaluates to false.
+ * - `malloc` fails.
+ */
+char *str_clone(char *s);
+
+/**
  * Obtains the character at `n`. Supports negative indexing.
  * Creates an internal copy of `str` so it isn't mutated.
  *
  * Returns `\0` when:
- * - `n > strlen(str)`
+ * - `n > str_len(str)`
  */
 char str_char_at(char *str, int n);
 
@@ -27,7 +44,7 @@ char str_char_at(char *str, int n);
  * Returns an array of pointers to strings with a length of `total`.
  *
  * Returns `NULL` when:
- * - `strdup` fails to duplicate `str`.
+ * - `str_clone` fails to duplicate `str`.
  * - `realloc` for `tokens` (internal variable) fails.
  */
 char **str_split(char *str, const char *sep, size_t *total);
@@ -37,9 +54,9 @@ char **str_split(char *str, const char *sep, size_t *total);
  * Creates an internal copy of `str` so it isn't mutated.
  *
  * Returns `NULL` when:
- * - `strdup` fails to duplicate `str`.
+ * - `str_clone` fails to duplicate `str`.
  * - `strtok` return a `NULL` token.
- * - `strdup` fails to duplicate `first` (returned value).
+ * - `str_clone` fails to duplicate `first` (returned value).
  */
 char *str_split_first(char *str, const char *sep);
 
@@ -48,9 +65,9 @@ char *str_split_first(char *str, const char *sep);
  * Creates an internal copy of `str` so it isn't mutated.
  *
  * Returns `NULL` when:
- * - `strdup` fails to duplicate `str`.
+ * - `str_clone` fails to duplicate `str`.
  * - `strtok` return a `NULL` token.
- * - `strdup` fails to duplicate `first` (returned value).
+ * - `str_clone` fails to duplicate `first` (returned value).
  */
 char *str_split_last(char *str, const char *sep);
 
@@ -82,8 +99,28 @@ char *str_concat_va(char *first, char *second, ...);
 #if !defined(__STR_IMPLEMENTED) && (defined(STRUCTYPES_IMPLEMENTATION) || defined(STR_IMPLEMENTATION))
 #define __STR_IMPLEMENTED
 
+size_t str_len(char *s) {
+    if (!s) return 0;
+
+    size_t len = 0;
+    while (s[len]) len++;
+    return len;
+}
+
+char *str_clone(char *s) {
+    if (!s) return NULL;
+
+    size_t len = str_len(s) + 1;
+    char *copy = malloc(len);
+    if (!copy) return NULL;
+
+    for (size_t i = 0; i < len; i++) copy[i] = s[i];
+
+    return copy;
+}
+
 char str_char_at(char *str, int n) {
-    int size = (int)strlen(str);
+    int size = (int)str_len(str);
 
     if (n > size) {
         printf("Index %d out of bounds for string of size %zu\n", n, size);
@@ -96,7 +133,7 @@ char str_char_at(char *str, int n) {
 }
 
 char **str_split(char *str, const char *sep, size_t *total) {
-    char *tmp = strdup(str);
+    char *tmp = str_clone(str);
     if (tmp == NULL) return NULL;
 
     char **tokens = NULL;
@@ -115,7 +152,7 @@ char **str_split(char *str, const char *sep, size_t *total) {
         }
 
         tokens = new_tokens;
-        tokens[cap++] = strdup(token);
+        tokens[cap++] = str_clone(token);
         token = strtok(NULL, sep);
     }
 
@@ -124,7 +161,7 @@ char **str_split(char *str, const char *sep, size_t *total) {
 }
 
 char *str_split_first(char *str, const char *sep) {
-    char *tmp = strdup(str);
+    char *tmp = str_clone(str);
     if (tmp == NULL) return NULL;
 
     char *token = strtok(tmp, sep);
@@ -133,14 +170,14 @@ char *str_split_first(char *str, const char *sep) {
         return NULL;
     }
 
-    char *first = strdup(token);
+    char *first = str_clone(token);
     free(tmp);
 
     return first;
 }
 
 char *str_split_last(char *str, const char *sep) {
-    char *tmp = strdup(str);
+    char *tmp = str_clone(str);
     if (tmp == NULL) return NULL;
 
     char *token = strtok(tmp, sep);
@@ -151,7 +188,7 @@ char *str_split_last(char *str, const char *sep) {
 
     char *last;
     while (token) {
-        last = strdup(token);
+        last = str_clone(token);
         token = strtok(NULL, sep);
     }
 
@@ -163,14 +200,14 @@ char *str_split_last(char *str, const char *sep) {
 char *str_concat(char **arr, size_t size) {
     if (!arr || !size) return NULL;
 
-    char *buf = malloc(strlen(arr[0]) + 1);
+    char *buf = malloc(str_len(arr[0]) + 1);
     if (!buf) return NULL;
 
     strcpy(buf, arr[0]);
 
-    size_t total = strlen(buf);
+    size_t total = str_len(buf);
     for (size_t i = 1; i < size; i++) {
-        size_t elementSize = strlen(arr[i]);
+        size_t elementSize = str_len(arr[i]);
 
         char *tmp = realloc(buf, total + elementSize + 1);
         if (!tmp) {
@@ -189,7 +226,7 @@ char *str_concat(char **arr, size_t size) {
 char *str_concat_va(char *first, char *second, ...) {
     if (first == NULL || second == NULL) return NULL;
 
-    char *buf = malloc(strlen(first) + strlen(second) + 1);
+    char *buf = malloc(str_len(first) + str_len(second) + 1);
     if (!buf) return NULL;
 
     strcpy(buf, first);
@@ -200,7 +237,7 @@ char *str_concat_va(char *first, char *second, ...) {
 
     char *arg;
     while ((arg = va_arg(args, char *))) {
-        char *tmp = realloc(buf, strlen(buf) + strlen(arg) + 1);
+        char *tmp = realloc(buf, str_len(buf) + str_len(arg) + 1);
         if (!tmp) {
             free(buf);
             va_end(args);

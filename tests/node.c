20a12f2a718f1cbae883374b7f445a5e1905edb9
Cristian Marcos Martin <cristianmarcosmartin1@gmail.com>
2025-11-08 23:11:12 +0100

    node.c: added tests for the previous node.h changes

--- tests/node.c
+++ tests/node.c
@@ -6,11 +6,11 @@
 
 // TODO free memory on tests
 
-bool node_new_ok_null_parent_and_value() {
-    TEST("node_new_ok_null_parent_and_value");
+bool node_new_ok() {
+    TEST("node_new_ok");
 
     char *value = "abcde";
-    Node *node = node_new(NULL, value);
+    Node *node = node_new(value);
 
     ASSERT_BLOCK(
         ASSERT_STR(node->value, value),
@@ -20,10 +20,10 @@ bool node_new_ok_null_parent_and_value() {
     );
 }
 
-bool node_new_ok_null_parent_and_null_value() {
-    TEST("node_new_ok_null_parent_and_null_value");
+bool node_new_ok_value_is_null() {
+    TEST("node_new_ok_value_is_null");
 
-    Node *node = node_new(NULL, NULL);
+    Node *node = node_new(NULL);
 
     ASSERT_BLOCK(
         ASSERT_NULL(node->value),
@@ -33,57 +33,64 @@ bool node_new_ok_null_parent_and_null_value() {
     );
 }
 
-bool node_new_ok_with_parent_and_value() {
-    TEST("node_new_ok_with_parent_and_value");
-
-    char *value = "abcde";
-    Node *parent = node_new(NULL, value);
-    Node *result = node_new(parent, value);
-
-    ASSERT_BLOCK(
-        ASSERT_STR(parent->value, value),
-        ASSERT_NULL(parent->parent),
-        ASSERT(parent->nodes[0] == result),
-        ASSERT(parent->size == 1),
-
-        ASSERT_STR(result->value, value),
-        ASSERT(result->parent == parent),
-        ASSERT_NULL(result->nodes),
-        ASSERT_ZERO(result->size),
-    );
-}
-
-bool node_new_ok_with_parent_and_null_value() {
-    TEST("node_new_ok_with_parent_and_null_value");
-
-    char *value = NULL;
-    Node *parent = node_new(NULL, value);
-    Node *result = node_new(parent, value);
-
-    ASSERT_BLOCK(
-        ASSERT_NULL(parent->value),
-        ASSERT_NULL(parent->parent),
-        ASSERT(parent->nodes[0] == result),
-        ASSERT(parent->size == 1),
-
-        ASSERT_NULL(result->value),
-        ASSERT(result->parent == parent),
-        ASSERT_NULL(result->nodes),
-        ASSERT_ZERO(result->size),
-    );
-}
-
 bool node_add_ok() {
     TEST("node_add_ok");
 
     char *value1 = "abcde";
     char *value2 = "12345";
 
-    Node *parent = node_new(NULL, value1);
-    Node *child = node_new(NULL, value2);
+    Node *parent = node_new(value1);
+    Node *child = node_add(parent, value2);
 
     ASSERT_BLOCK(
-        ASSERT_TRUE(node_add(parent, child)),
+        ASSERT_STR(parent->value, value1),
+        ASSERT_NULL(parent->parent),
+        ASSERT(parent->nodes[0] == child),
+        ASSERT(parent->size == 1),
+
+        ASSERT_STR(child->value, value2),
+        ASSERT(child->parent == parent),
+        ASSERT_NULL(child->nodes),
+        ASSERT_ZERO(child->size),
+    );
+}
+
+bool node_add_ok_value_is_null() {
+    TEST("node_add_ok_value_is_null");
+
+    Node *parent = node_new(NULL);
+    Node *child = node_add(parent, NULL);
+
+    ASSERT_BLOCK(
+        ASSERT_NULL(parent->value),
+        ASSERT_NULL(parent->parent),
+        ASSERT(parent->nodes[0] == child),
+        ASSERT(parent->size == 1),
+
+        ASSERT_NULL(child->value),
+        ASSERT(child->parent == parent),
+        ASSERT_NULL(child->nodes),
+        ASSERT_ZERO(child->size),
+    );
+}
+
+bool node_add_err_parent_is_null() {
+    TEST("node_add_err_parent_is_null");
+
+    ASSERT_NULL(node_add(NULL, "abcde"));
+}
+
+bool node_add_child_ok() {
+    TEST("node_add_child_ok");
+
+    char *value1 = "abcde";
+    char *value2 = "12345";
+
+    Node *parent = node_new(value1);
+    Node *child = node_new(value2);
+
+    ASSERT_BLOCK(
+        ASSERT_TRUE(node_add_child(parent, child)),
 
         ASSERT_STR(parent->value, value1),
         ASSERT_NULL(parent->parent),
@@ -97,27 +104,31 @@ bool node_add_ok() {
     );
 }
 
-bool node_add_err_parent_is_null() {
-    TEST("node_add_err_parent_is_null");
+bool node_add_child_err_parent_is_null() {
+    TEST("node_add_child_err_parent_is_null");
 
-    Node *child = node_new(NULL, "abcde");
-
-    ASSERT_FALSE(node_add(NULL, child));
+    ASSERT_FALSE(node_add_child(NULL, node_new("abcde")));
 }
 
-bool node_add_err_child_is_null() {
-    TEST("node_add_err_child_is_null");
+bool node_add_child_err_child_is_null() {
+    TEST("node_add_child_err_child_is_null");
 
-    Node *parent = node_new(NULL, "abcde");
+    ASSERT_FALSE(node_add_child(node_new("abcde"), NULL));
+}
 
-    ASSERT_FALSE(node_add(parent, NULL));
+bool node_add_child_err_parent_eq_child() {
+    TEST("node_add_child_err_parent_eq_child");
+
+    Node *node = node_new("abcde");
+
+    ASSERT_FALSE(node_add_child(node, node));
 }
 
 bool node_print_ok() {
     TEST("node_print_ok");
 
-    Node *parent = node_new(NULL, "abcde");
-    Node *child = node_new(parent, "12345");
+    Node *parent = node_new("abcde");
+    node_add(parent, "12345");
 
     ASSERT_TRUE(node_print(parent, nodeprintopts_default()));
 }
@@ -125,7 +136,7 @@ bool node_print_ok() {
 bool node_print_ok_node_size_is_0() {
     TEST("node_print_ok_node_size_is_0");
 
-    Node *node = node_new(NULL, "abcde");
+    Node *node = node_new("abcde");
 
     ASSERT_TRUE(node_print(node, nodeprintopts_default()));
 }
@@ -139,7 +150,7 @@ bool node_print_err_node_is_null() {
 bool node_print_err_opts_indent_step_lt_1() {
     TEST("node_print_err_opts_indent_step_lt_1");
 
-    Node *node = node_new(NULL, "abcde");
+    Node *node = node_new("abcde");
     NodePrintOpts opts = nodeprintopts_default();
     opts.indent_step = 0;
 
@@ -152,8 +163,8 @@ bool node_print_deep_ok() {
     char *value1 = str_clone("abcde");
     char *value2 = str_clone("12345");
 
-    Node *parent = node_new(NULL, value1);
-    Node *child = node_new(parent, value2);
+    Node *parent = node_new(value1);
+    node_add(parent, value2);
 
     ASSERT_TRUE(node_print_deep(parent));
 }
@@ -167,7 +178,7 @@ bool node_print_deep_err_node_is_null() {
 bool node_print_shallow_ok() {
     TEST("node_print_shallow_ok");
 
-    Node *node = node_new(NULL, "abcde");
+    Node *node = node_new("abcde");
 
     ASSERT_TRUE(node_print_shallow(node));
 }
@@ -181,7 +192,7 @@ bool node_print_shallow_err_node_is_null() {
 bool node_info_ok() {
     TEST("node_info_ok");
 
-    ASSERT_TRUE(node_info(node_new(NULL, "abcde")));
+    ASSERT_TRUE(node_info(node_new("abcde")));
 }
 
 bool node_info_err_node_is_null() {
@@ -193,9 +204,9 @@ bool node_info_err_node_is_null() {
 bool node_total_ok() {
     TEST("node_total_ok");
 
-    Node *parent = node_new(NULL, "abcde");
-    Node *child1 = node_new(parent, "123");
-    Node *child2 = node_new(parent, "456");
+    Node *parent = node_new("abcde");
+    node_add(parent, "123");
+    node_add(parent, "456");
 
     ASSERT(node_total(parent) == 2);
 }
@@ -203,7 +214,7 @@ bool node_total_ok() {
 bool node_total_ok_without_nodes() {
     TEST("node_total_ok_without_nodes");
 
-    ASSERT_ZERO(node_total(node_new(NULL, "abcde")));
+    ASSERT_ZERO(node_total(node_new("abcde")));
 }
 
 bool node_total_err_node_is_null() {
@@ -215,9 +226,9 @@ bool node_total_err_node_is_null() {
 bool node_free_ok() {
     TEST("node_free_ok");
 
-    Node *parent = node_new(NULL, str_clone("abcde"));
-    Node *child1 = node_new(parent, str_clone("123"));
-    Node *child2 = node_new(parent, str_clone("456"));
+    Node *parent = node_new(str_clone("abcde"));
+    node_add(parent, str_clone("123"));
+    node_add(parent, str_clone("456"));
 
     ASSERT_TRUE(node_free(parent));
 }
@@ -231,7 +242,7 @@ bool node_free_err_node_is_null() {
 bool node_free_err_no_nodes() {
     TEST("node_free_err_no_nodes");
 
-    Node *node = node_new(NULL, "abcde");
+    Node *node = node_new("abcde");
     node->size = 1;
 
     ASSERT_FALSE(node_free(node));
@@ -252,15 +263,19 @@ bool nodeprintopts_default_ok() {
 int main() {
     TEST_SUITE(
         // node_new
-        node_new_ok_null_parent_and_value,
-        node_new_ok_null_parent_and_null_value,
-        node_new_ok_with_parent_and_value,
-        node_new_ok_with_parent_and_null_value,
+        node_new_ok,
+        node_new_ok_value_is_null,
 
         // node_add
         node_add_ok,
+        node_add_ok_value_is_null,
         node_add_err_parent_is_null,
-        node_add_err_child_is_null,
+
+        // node_add_child
+        node_add_child_ok,
+        node_add_child_err_parent_is_null,
+        node_add_child_err_child_is_null,
+        node_add_child_err_parent_eq_child,
 
         // node_print
         node_print_ok,
